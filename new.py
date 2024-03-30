2d685ede15b28d2257724c4a4fae6347eab46bb7
sreemahi-code <115690831+sreemahi-code@users.noreply.github.com>
2024-03-30 19:33:15 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,52 @@
+import random
+import json
+import pickle
+import numpy as np
+import tensorflow as tf 
+
+import nltk 
+from nltk.stem import WorldNetLemmatizer
+
+lemmatizer = WorldNetLemmatizer()
+
+intents = json.loads(open('intents.json').read())
+
+words  = []
+classes = []
+documents = []
+ignoreLetters = ['?','!','.',',']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+    wordList = nltk.word_tokenize(pattern)
+    words.extend(wordList)
+    documents.append((wordList, intent['tag']))
+    if intent['tag'] not in classes:
+        classes.append(intent['tag'])
+    
+words = [lemmatizer.lemmatize(word)for word in words if word not in ignoreLetters]
+words = sorted(set(classes))
+
+classes = sorted(set(classes))
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(words, open('classes.pkl', 'wb'))
+
+training = []
+outputEmpty = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    wordPattern = document[0]
+    wordPatterns = [lemmatizer.lemmatize(word.lower())for word in wordPatterns]
+    for word in words: bag.append(1) if word in wordPattern else bag.append(0)
+
+    outputRow = list(outputEmpty) 
+    outputRow[classes.indexdocument[1]] = 1
+    training.append(bag + outputRow)
+
+random.shuffle(training)
+training = np.array(training)
+
+trainX = training[: , :len(words)]
+trainY = traning [ ;, : len(words)]
